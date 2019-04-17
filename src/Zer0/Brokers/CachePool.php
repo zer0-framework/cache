8af6d6ebafcce92b4287095c7376777dcb85428f
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2019-04-17 17:59:22 +0800

    Typing

--- src/Zer0/Brokers/CachePool.php
+++ src/Zer0/Brokers/CachePool.php
@@ -20,4 +20,14 @@ class CachePool extends Base
         $class = ClassFinder::find($config->type, ClassFinder::getNamespace(\Zer0\Cache\Pools\Base::class), '~');
         return new $class($config, $this->app);
     }
+
+    /**
+     * @param string $name
+     * @param bool $caching
+     * @return \Zer0\Cache\Pools\Base
+     */
+    public function get(string $name = '', bool $caching = true): \Zer0\Cache\Pools\Base
+    {
+        return parent::get($name, $caching);
+    }
 }
