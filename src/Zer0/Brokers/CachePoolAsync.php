aefe24d9dc1551fa0b8a7199486e4722d7a9b5a7
Vasily Zorin <kak.serpom.po.yaitsam@gmail.com>
2019-04-17 17:53:48 +0800

    More cowbell

--- src/Zer0/Brokers/CachePoolAsync.php
+++ src/Zer0/Brokers/CachePoolAsync.php
@@ -25,4 +25,14 @@ class CachePoolAsync extends Base
         $class = ClassFinder::find($config->type . 'Async', ClassFinder::getNamespace(\Zer0\Cache\Pools\BaseAsync::class), '~');
         return new $class($config, $this->app);
     }
+
+    /**
+     * @param string $name
+     * @param bool $caching
+     * @return \Zer0\Cache\Pools\BaseAsync
+     */
+    public function get(string $name = '', bool $caching = true): \Zer0\Cache\Pools\BaseAsync
+    {
+        return parent::get($name, $caching);
+    }
 }
